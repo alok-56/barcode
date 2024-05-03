7e5c7705f09aaf55ff9596e8fe131fc50b87a758
alok-56 <alokkumar11746@gmail.com>
2024-05-03 09:49:15 +0530

    helo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React, { useState } from "react";
-import { BarcodeScanner, useTorch } from "react-barcode-scanner";
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 
 const App = () => {
-  const [isSupportTorch, isOpen, onTorchSwitch] = useTorch();
-  const [selectedCamera, setSelectedCamera] = useState("environment");
-
-  const handleCameraChange = (camera) => {
-    setSelectedCamera(camera);
-  };
-
+  const [data, setData] = useState("");
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <div style={{ width: "100%", height: "360px", position: "relative" }}>
-        <BarcodeScanner facingMode={selectedCamera} />
-        {isSupportTorch ? (
-          <button style={{ position: "absolute", top: "10px", left: "10px" }} onClick={onTorchSwitch}>
-            Switch Torch
-          </button>
-        ) : null}
-      </div>
-      <div style={{ marginTop: "20px" }}>
-        <label>Select Camera:</label>
-        <select value={selectedCamera} onChange={(e) => handleCameraChange(e.target.value)}>
-          <option value="environment">Back Camera</option>
-          <option value="user">Front Camera</option>
-        </select>
-      </div>
-    </div>
+    <>
+      <Scanner
+        onResult={(text, result) => setData(result)}
+        onError={(error) => console.log(error?.message)}
+      />
+      <h1>{data}</h1>
+    </>
   );
 };
 
